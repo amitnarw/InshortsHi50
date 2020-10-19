f584adb811865592a7cc634ded58f1a1c539c7b9
amitnarw <71306477+amitnarw@users.noreply.github.com>
2020-10-19 18:07:39 +0530

    Update endpoints.js

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,18 +1,18 @@
 module.exports = (req, res) => {
   let endpoints = {
-    all: "https://inshot.herokuapp.com/v1/all",
-    national: "https://inshot.herokuapp.com/v1/national",
-    business: "https://inshot.herokuapp.com/v1/business",
-    sports: "https://inshot.herokuapp.com/v1/sports",
-    world: "https://inshot.herokuapp.com/v1/world",
-    politics: "https://inshot.herokuapp.com/v1/politics",
-    technology: "https://inshot.herokuapp.com/v1/technology",
-    startup: "https://inshot.herokuapp.com/v1/startup",
-    entertainment: "https://inshot.herokuapp.com/v1/entertainment",
-    science: "https://inshot.herokuapp.com/v1/science",
-    automobile: "https://inshot.herokuapp.com/v1/automobile",
-    miscellaneous: "https://inshot.herokuapp.com/v1/miscellaneous",
-    hatke: "https://inshot.herokuapp.com/v1/hatke",
+    all: "https://inshortshi50.vercel.app/all",
+    national: "https://inshortshi50.vercel.app/national",
+    business: "https://inshortshi50.vercel.app/business",
+    sports: "https://inshortshi50.vercel.app/sports",
+    world: "https://inshortshi50.vercel.app/world",
+    politics: "https://inshortshi50.vercel.app/politics",
+    technology: "https://inshortshi50.vercel.app/technology",
+    startup: "https://inshortshi50.vercel.app/startup",
+    entertainment: "https://inshortshi50.vercel.app/entertainment",
+    science: "https://inshortshi50.vercel.app/science",
+    automobile: "https://inshortshi50.vercel.app/automobile",
+    miscellaneous: "https://inshortshi50.vercel.app/miscellaneous",
+    hatke: "https://inshortshi50.vercel.app/hatke",
   };
 
   res.json(endpoints);
